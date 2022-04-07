0541dcaeb5c6decece086d2c4a5429beab2b6046
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-07 13:35:47 +0430

    cartable_screen created

---
+++ lib/view/screens/cartable_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CartableScreen extends StatelessWidget {
+  const CartableScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreenAccent,
+      child: Center(child: Text('Cartable Screen')),
+    );
+  }
+}
