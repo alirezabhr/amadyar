895a981051bbff4d79ad999d2ecb924259fa1282
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-07 14:17:10 +0430

    order_hisotry_page.dart file and widget name changed to OrdersPage

--- lib/view/screens/orders_screen.dart
+++ lib/view/screens/orders_screen.dart
@@ -2,8 +2,8 @@ import 'package:amadyar/view/widgets/order_history_card.dart';
 import 'package:flutter/material.dart';
 import '../../dummy_data/dummy_orders.dart';
 
-class HistoryPage extends StatelessWidget {
-  const HistoryPage({Key? key}) : super(key: key);
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
