a0e10e2bb3ce7dec4b09b422474e5ea347d03b29
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-07 12:41:26 +0430

    color Schema changed.

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'حمل و نقل آمادیار',
-        theme: ThemeData(
-          fontFamily: 'SamimFD',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.green,
-          ).copyWith(
-            secondary: const Color.fromRGBO(34, 202, 21, 1),
-          ),
+      title: 'حمل و نقل آمادیار',
+      theme: ThemeData(
+        fontFamily: 'SamimFD',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+            secondary: const Color(0xffb0d4f4),
+            tertiary: const Color(0xff3461f3)
         ),
-        builder: (context, child) {
-          return Directionality(
-            textDirection: TextDirection.rtl,
-            child: Container(child: child),
-          );
-        },
-        initialRoute: PageRoutes.welcomeScreen,
-        routes: {
-          PageRoutes.welcomeScreen: (ctx) => WelcomeScreen(),
-          PageRoutes.mainPage: (ctx) => const MainPage(),
-        });
+      ),
+      builder: (context, child) {
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: Container(child: child),
+        );
+      },
+      initialRoute: PageRoutes.welcomeScreen,
+      routes: {
+        PageRoutes.welcomeScreen: (ctx) => WelcomeScreen(),
+        PageRoutes.mainPage: (ctx) => MainPage(),
+      },
+    );
   }
 }
