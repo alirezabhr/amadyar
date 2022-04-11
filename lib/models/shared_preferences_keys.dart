325072ad4f06767ab869660ba1e14719b0a606c3
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-11 17:30:30 +0430

    shared preferences keys added in a file

---
+++ lib/models/shared_preferences_keys.dart
@@ -0,0 +1,7 @@
+class SharedPreferencesKeys {
+  static const String phoneNumber = "phoneNumber";
+  static const String firstname = "firstname";
+  static const String lastname = "lastname";
+  static const String company = "company";
+  static const String token = "token";
+}
