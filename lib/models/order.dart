5f827d7e3d5e380ef550d7d901c564705b30f1d5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-11 23:46:54 +0430

    order Status type changed in constructor

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -16,25 +16,45 @@ class Order with ChangeNotifier {
   Storage? storage;
   Store? store;
   String title;
-  OrderStatus status;
+  late OrderStatus status;
   int weight;
   DateTime startTw;
   DateTime endTw;
   late Jalali estimationArrival;
   late Jalali estimationDepart;
 
+  OrderStatus _convertStrToStatus(String strStatus) {
+    OrderStatus s = OrderStatus.ASSIGNED;
+    switch (strStatus) {
+      case 'AS':
+        s = OrderStatus.ASSIGNED;
+        break;
+      case 'IP':
+        s = OrderStatus.IN_PROGRESS;
+        break;
+      case 'AR':
+        s = OrderStatus.ARRIVED;
+        break;
+      case 'DL':
+        s = OrderStatus.DELIVERED;
+        break;
+    }
+    return s;
+  }
+
   Order({
     required this.id,
     this.storage,
     this.store,
     required this.title,
-    required this.status,
+    required String statusText,
     required this.weight,
     required this.startTw,
     required this.endTw,
     required DateTime estimationArrival,
     required DateTime estimationDepart,
   }) {
+    status = _convertStrToStatus(statusText);
     this.estimationArrival = Jalali.fromDateTime(estimationArrival);
     this.estimationDepart = Jalali.fromDateTime(estimationDepart);
   }
