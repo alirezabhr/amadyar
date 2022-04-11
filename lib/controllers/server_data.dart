47f5ee8410f25b20fb3cbe24ebc8700ce9a27989
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-11 23:45:57 +0430

    baseUrl and token added in Dio options in ServerData Dio instance

--- lib/controllers/server_data.dart
+++ lib/controllers/server_data.dart
@@ -1,3 +1,23 @@
+import 'package:dio/dio.dart';
+
+import 'auth.dart';
+
 class ServerData {
   static String serverBaseAPI = 'http://10.0.2.2:8000';
+
+  final String baseUrl = 'http://10.0.2.2:8000/';
+  final Dio _dio = Dio();
+
+  ServerData () {
+    _dio.options.baseUrl = baseUrl;
+  }
+
+  Future<Dio> getDio () async {
+    String? token = await Auth.getToken();
+    if (token != null) {
+      _dio.options.headers['Authorization'] = "Token $token";
+    }
+    return _dio;
+  }
+
 }
