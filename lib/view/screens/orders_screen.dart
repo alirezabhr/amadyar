79ee53c3f0113a68dbe6b8229ef5c94d0c7996de
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-12 10:39:47 +0430

    provider and refresh indicator added

--- lib/view/screens/orders_screen.dart
+++ lib/view/screens/orders_screen.dart
@@ -1,17 +1,24 @@
 import 'package:amadyar/view/widgets/order_history_card.dart';
 import 'package:flutter/material.dart';
-import '../../dummy_data/dummy_orders.dart';
+import 'package:provider/provider.dart';
+import '../../controllers/history_orders_provider.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: DummyOrders.items.length,
-      itemBuilder: (BuildContext context, int index) {
-          return OrderHistoryCard(DummyOrders.items[index]);
-        },
-      );
+    final HistoryOrdersProvider ordersProvider =
+    Provider.of<HistoryOrdersProvider>(context);
+
+    return RefreshIndicator(
+      onRefresh: ordersProvider.updateOrders,
+      child: ListView.builder(
+        itemCount: ordersProvider.orders.length,
+        itemBuilder: (BuildContext context, int index) {
+            return OrderHistoryCard(ordersProvider.orders[index]);
+          },
+        ),
+    );
   }
 }
