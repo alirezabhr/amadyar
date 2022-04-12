83893a028a76d8457ee7d7e23bcb06518e4686ae
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-04-12 14:42:51 +0430

    tracker added to save OrderLogs and submit it when process finished

--- lib/controllers/map_provider.dart
+++ lib/controllers/map_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:amadyar/controllers/server_data.dart';
 import 'package:amadyar/models/order.dart';
+import 'package:amadyar/models/order_log.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -7,6 +10,9 @@ import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 
 class MapProvider with ChangeNotifier {
+  Timer? _timer;
+  List<OrderLog> _orderLogs = [];
+
   late Order _nextOrder;
   late List<LatLng> _nextOrderPath;
 
@@ -72,6 +78,58 @@ class MapProvider with ChangeNotifier {
       }
     }
     return await Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _addLog(int orderId, LogAction action) async {
+    Position? position = await _getCurrentPosition();
+    double _lat = 0;
+    double _long = 0;
+    if (position != null) {
+      _lat = position.latitude;
+      _long = position.longitude;
+    }
+    _orderLogs.add(
+      OrderLog(
+        orderId: orderId,
+        latitude: _lat,
+        longitude: _long,
+        action: action,
+        dateTime: DateTime.now(),
+      ),
+    );
+  }
+
+  void runTracker(int orderId) async {
+    _orderLogs = [];
+    await _addLog(orderId, LogAction.STARTED);
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 3),
+      (timer) async {
+        await _addLog(orderId, LogAction.ON_THE_WAY);
+      },
+    );
+  }
+
+  void stopTracker(int orderId) async {
+    _timer?.cancel();
+    await _addLog(orderId, LogAction.ENDED);
+
+    List<Map<String, dynamic>> _logsData = [];
+    for (OrderLog log in _orderLogs) {
+      _logsData.add(log.toMap());
+    }
+
+    await _submitOrderLogs(_logsData);
+  }
+
+  Future<void> _submitOrderLogs(List<Map> data) async {
+    Dio dio = await ServerData().getDio();
+    try {
+      await dio.post('/haul/order/uncompleted/', data: data);
+    } catch (e) {
+      print('problem in submitting order logs');
+    }
   }
 
   void changeMyLocation(double lat, double long) {
